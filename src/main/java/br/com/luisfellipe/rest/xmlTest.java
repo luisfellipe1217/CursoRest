946795a83031d47c507e9c3dd696aa1d9e76f1d5
Luis fellipe <luis.fellipe1217@gmail.com>
2021-05-26 15:09:20 -0300

    aula 20

--- src/main/java/br/com/luisfellipe/rest/xmlTest.java
+++ src/main/java/br/com/luisfellipe/rest/xmlTest.java
@@ -18,13 +18,31 @@ public class xmlTest {
 					.get("http://restapi.wcaquino.me/usersXML/3")
 				.then()
 					.statusCode(200)
-					.body("user.name", is("Ana Julia"))
-					.body("user.@id", is("3"))				
-					.body("user.filhos.name[0]", is("Zezinho"))
-					.body("user.filhos.name[1]",is("Luizinho"))
-					.body("user.filhos.name", hasItem("Luizinho"))
-					.body("user.filhos.name", hasItems("Luizinho", "Zezinho"))				
-					
+					.rootPath("user")
+					.body("name", is("Ana Julia"))
+					.body("@id", is("3"))				
+					.body("filhos.name[0]", is("Zezinho"))
+					.body("filhos.name[1]",is("Luizinho"))
+					.body("filhos.name", hasItem("Luizinho"))
+					.body("filhos.name", hasItems("Luizinho", "Zezinho"))
+		;
+	}
+	
+	@Test
+	public void devoFazerPesquisasAvançadasXML() {
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/usersXML/")
+		.then()
+			.statusCode(200)
+			.body("users.user.size()", is(3))
+			.body("users.user.findAll{it.age.toInteger() <=  25}.size()", is(2)) 
+			.body("users.user.@id",hasItems("1", "2", "3"))
+			.body("users.user.find{it.age == 25}.name", is("Maria Joaquina"))
+			.body("users.user.findAll{it.name.toString().contains('n')}.name", hasItems("Maria Joaquina", "Ana Julia"))
+			.body("users.user.salary.find{it != null}.toDouble()", is(1234.5678d))
+			.body("users.user.age.collect{it.toInteger() *2}", hasItems(40,50,60)) 
+			.body("users.user.name.findAll{it.toString().startsWith('Maria')}.collect{it.toString().toUpperCase()}", is("MARIA JOAQUINA"))
 		;
 	}
 	
