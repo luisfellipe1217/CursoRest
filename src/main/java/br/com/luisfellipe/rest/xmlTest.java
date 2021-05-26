96a1ea2a94db0ef77c1585dd05d7e7a8ef6c37e4
Luis fellipe <luis.fellipe1217@gmail.com>
2021-05-26 15:24:26 -0300

    Aula 21

--- src/main/java/br/com/luisfellipe/rest/xmlTest.java
+++ src/main/java/br/com/luisfellipe/rest/xmlTest.java
@@ -3,8 +3,12 @@ package br.com.luisfellipe.rest;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
+import java.util.ArrayList;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import io.restassured.internal.path.xml.NodeImpl;
 
 
 
@@ -44,6 +48,20 @@ public class xmlTest {
 			.body("users.user.age.collect{it.toInteger() *2}", hasItems(40,50,60)) 
 			.body("users.user.name.findAll{it.toString().startsWith('Maria')}.collect{it.toString().toUpperCase()}", is("MARIA JOAQUINA"))
 		;
+	}
+	
+	@Test
+	public void devoFazerPesquisasAvancadasComXMLeJava() {
+		ArrayList<NodeImpl> nomes = given()
+		.when()
+			.get("http://restapi.wcaquino.me/usersXML/")
+		.then()
+			.statusCode(200)
+			.extract().path("users.user.name.findAll{it.toString().contains('n')}")
+		;
+		Assert.assertEquals(2, nomes.size());
+		Assert.assertEquals("Maria Joaquina".toUpperCase(), nomes.get(0).toString().toUpperCase());
+		Assert.assertTrue("ANA JULIA".equalsIgnoreCase(nomes.get(1).toString()));
 	}
 	
 }
