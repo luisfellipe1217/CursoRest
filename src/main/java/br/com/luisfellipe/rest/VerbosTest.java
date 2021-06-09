3868613d2df87ffd7f7fa265ca2d079d2adfe3ef
Luis fellipe <luis.fellipe1217@gmail.com>
2021-06-09 14:08:00 -0300

    Serializando com MAP

--- src/main/java/br/com/luisfellipe/rest/VerbosTest.java
+++ src/main/java/br/com/luisfellipe/rest/VerbosTest.java
@@ -3,12 +3,17 @@ package br.com.luisfellipe.rest;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Test;
 
 import io.restassured.http.ContentType;
 
 public class VerbosTest {
+	
+	private static String apiToken = "b056fbc4609acfa4d3241ce1a6fe725bfa267badb3996abf518851b8db92d0ab"; //Deve ser informado o Token do usuário trello
+	private static String apiKey = "f560501b8207ba3a8632de34b00e8a60"; // Deve ser informado a key da api trello
 
 	@Test
 	public void deveSalvarUsuario() {
@@ -24,6 +29,27 @@ public class VerbosTest {
 			.body("id", is(notNullValue()))
 			.body("name", is("Jose"))
 			.body("age", is(50))
+		;		
+	}
+	
+	@Test
+	public void deveSalvarUsuarioMap() {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("name", "Usuario via MAP");
+		params.put("age", 25);
+		
+		given()
+			.log().all()
+			.contentType("application/json")
+			.body(params)
+		.when()
+			.post("http://restapi.wcaquino.me/users")
+		.then()
+			.log().all()
+			.statusCode(201)
+			.body("id", is(notNullValue()))
+			.body("name", is("Usuario via MAP"))
+			.body("age", is(25))
 		;		
 	}
 	
@@ -56,10 +82,37 @@ public class VerbosTest {
 		.statusCode(201)
 		.body("user.@id", is(notNullValue()))		
 		.body("user.name", is("Jose"))
-		.body("user.age", is("50"))
-		
+		.body("user.age", is("50"))		
 		;	
 	}
 	
+	@Test
+	public void deveAlterarUsuario() {
+		given()
+		.log().all()
+		.contentType("application/json")
+		.body("{ \"name\": \"Nome Alterado\", 	\"age\": 80 }")
+	.when()
+		.put("http://restapi.wcaquino.me/users/1")
+	.then()
+		.log().all()		
+		.statusCode(200)
+		.body("id", is(1))
+		.body("name", is("Nome Alterado"))
+		.body("age", is("80"))	
+		;		
+	}
+	
+	@Test
+	public void queEstouComAcessoAoTrello() {
+				
+		given()
+			.log().method()
+			.log().uri()
+		.when()
+			.get("/1/members/me/boards?key=?key={yourKey}&token={yourToken}", apiKey , apiToken)
+		.then()
+		.statusCode(200);  //valida statusCode se requisição está acontecendo com sucesso.
+	}
 
 }
